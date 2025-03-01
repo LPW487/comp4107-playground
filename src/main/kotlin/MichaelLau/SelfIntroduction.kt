d1c461ccd21ed60b1999e75bfab342cc8a2b3d63
LPW487 <zxc254618@gmail.com>
2025-03-01 12:58:25 +0800

    Add self-introduction feature

---
+++ src/main/kotlin/MichaelLau/SelfIntroduction.kt
@@ -0,0 +1,10 @@
+package hk.edu.hkbu.comp.comp4107.playground.MichaelLau
+
+class SelfIntroduction {
+    fun selfIntroduction() {
+        println("Hello, my name is Michael Lau.")
+        println("I am a student at HKBU.")
+        println("I am studying Computer Science.")
+        println("I am interested in machine learning.")
+    }
+}
